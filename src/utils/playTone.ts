48dcbaac54a715dfd78b17beae61fe2f089023d7
harryli0088 <harryli0088@gmail.com>
2021-06-23 17:12:50 -0400

    volume

--- src/utils/playTone.ts
+++ src/utils/playTone.ts
@@ -1,6 +1,7 @@
 import { now as toneNow, PolySynth, Synth } from 'tone'
 
 const synth = new PolySynth(Synth).toDestination()
+synth.volume.value = -7
 
 export default function playTone(
   note: string[] = ["440"],
