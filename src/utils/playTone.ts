9cb52c848a5281cfb07718ec6966a934ff2d5563
harryli0088 <harryli0088@gmail.com>
2021-06-24 10:16:25 -0400

    play tone tweak

--- src/utils/playTone.ts
+++ src/utils/playTone.ts
@@ -1,11 +1,10 @@
 import { now as toneNow, PolySynth, Synth } from 'tone'
 
-const synth = new PolySynth(Synth).toDestination()
-synth.volume.value = -10
-
 export default function playTone(
   note: string[] = ["440"],
   duration: string = "2n",
 ) {
+  const synth = new PolySynth(Synth).toDestination()
+  synth.volume.value = -10
   synth.triggerAttackRelease(note, duration, toneNow())
 }
