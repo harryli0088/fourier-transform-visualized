72aee4e81455fc53f49ea3d5ba82de355f5a3b08
harryli0088 <harryli0088@gmail.com>
2021-06-30 16:48:10 -0400

    dynamic timespan

--- src/Containers/DFT.svelte
+++ src/Containers/DFT.svelte
@@ -13,6 +13,9 @@
   import getPoints from '../utils/getPoints';
   import plural from '../utils/plural';
 
+  let domain: [number, number] = [DOMAIN[0], DOMAIN[1]]
+  $: timeSpan = domain[1] - domain[0]
+
   const SHIFTED = false
   const INITIAL_FUNC_FREQ = 2
   const funcFreq = tweened(INITIAL_FUNC_FREQ, { //this is the partial frequency for 2pi omitted for simplicity
@@ -21,20 +24,21 @@
   })
   $: fullFuncFreq = TWO_PI * $funcFreq //multiply by 2pi to get the full frequency to use
   $: func = getCos(fullFuncFreq, 0, SHIFTED)
-  $: points = getPoints(DOMAIN, func)
+  $: points = getPoints(domain, func)
 
-  $: zeroPaddedPoints = getPoints(DOMAIN, func, 4096).map(
+  const sampleOptions = [512, 1024, 2048, 4096]
+  let numSamples = 4096
+  $: zeroPaddedPoints = getPoints(domain, func, numSamples).map(
     p => complex.makeNew({r: p.y})
   )
-  $: span = DOMAIN[1] - DOMAIN[0]
   $: dftPoints = fft(zeroPaddedPoints).map((n,i) => ({
-    x: i/span,
+    x: i/timeSpan,
     y: complex.magnitude(n) / points.length
-  })).slice(0, zeroPaddedPoints.length / 2).slice(0, 10*span + 1)
+  })).slice(0, zeroPaddedPoints.length / 2).slice(0, 10*timeSpan + 1)
   $: console.log("zeroPaddedPoints.length",zeroPaddedPoints.length)
 
   $: cosineFourierTransform = getCosineFourierTransform(fullFuncFreq, 0, SHIFTED)
-  $: definiteIntegralFunction = getDefiniteIntegralFunction(cosineFourierTransform, DOMAIN[0], DOMAIN[1])
+  $: definiteIntegralFunction = getDefiniteIntegralFunction(cosineFourierTransform, domain[0], domain[1])
   $: getReal = (freq: number) => definiteIntegralFunction(freq).r
   $: ftPoints = getPoints([0, 10], getReal)
 </script>
@@ -43,7 +47,18 @@
   <h2>Discrete Fourier Transform</h2>
   <p></p>
   <div>
-    <span><b>Function Frequency: </b>{$funcFreq.toFixed(1)} {plural($funcFreq, "beat")} per second (Hz)</span>
+    <timeSpan><b>Time Span: </b>{timeSpan.toFixed(1)} {plural(timeSpan, "second")}</timeSpan>
+    <input
+      max="10"
+      min="1"
+      on:input={e => domain[1] = parseFloat(e.target.value)}
+      step="0.1"
+      type="range"
+      value={DOMAIN[1]}
+    />
+  </div>
+  <div>
+    <timeSpan><b>Function Frequency: </b>{$funcFreq.toFixed(1)} {plural($funcFreq, "beat")} per second (Hz)</timeSpan>
     <input
       max={WINDING_FREQ_MAX}
       min="0.05"
@@ -55,6 +70,15 @@
   </div>
   <Plot {points} xTitle="Time in seconds"/>
   <Plot points={ftPoints} xTitle="Fourier Transform"/>
+
+  <div>
+    <b>Number of Samples: </b>
+    <select bind:value={numSamples}>
+      {#each sampleOptions as s}
+        <option value={s}>{s}</option>
+      {/each}
+    </select>
+  </div>
 	<Plot points={dftPoints} xTitle="Discrete Fourier Transform"/>
 </main>
 
